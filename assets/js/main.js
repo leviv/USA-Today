b6e39eb39d0fc69f977297151f637a1a22dc8fb7
leviv <villarreallevi@gmail.com>
2018-11-02 15:29:51 -0500

    Correct the offsets

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,30 @@
 $(document).ready(function() {
-  if ($(document).scrollTop() > 100) {
+  chageCSS();
+
+  $(window).scroll(function() {
+    chageCSS();
+  });
+});
+
+function chageCSS() {
+  if ($(document).scrollTop() > 80) {
       $('.hide').css("display", "none");
       $('.nav-logo').show();
       $("nav").css("position", "fixed");
       $("#masthead").hide();
+      $("#social-sidebar").css("position", "fixed");
+      $("#social-sidebar").css("top", "39px");
+      $("#black-bars").css("position", "fixed");
+      $("#black-bars").css("top", "119px");
   }
   else {
       $('.hide').css("display", "inline");
       $('.nav-logo').hide();
       $("nav").css("position", "absolute");
       $("#masthead").show();
+      $("#social-sidebar").css("position", "absolute");
+      $("#social-sidebar").css("top", "120px");
+      $("#black-bars").css("position", "absolute");
+      $("#black-bars").css("top", "210px");
   }
-
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 100) {
-            $('.hide').css("display", "none");
-            $('.nav-logo').show();
-            $("nav").css("position", "fixed");
-            $("#masthead").hide();
-            $("#social-sidebar").css("position", "fixed");
-            $("#social-sidebar").css("top", "0px");
-            $("#black-bars").css("position", "fixed");
-            $("#black-bars").css("top", "90px");
-        }
-        else {
-            $('.hide').css("display", "inline");
-            $('.nav-logo').hide();
-            $("nav").css("position", "absolute");
-            $("#masthead").show();
-            $("#social-sidebar").css("position", "absolute");
-            $("#social-sidebar").css("top", "120px");
-            $("#black-bars").css("position", "absolute");
-            $("#black-bars").css("top", "210px");
-        }
-    });
-});
+}
