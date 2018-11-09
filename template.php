2f06a95b31fe9e13939af9d42f618336f819593a
leviv <villarreallevi@gmail.com>
2018-11-09 13:26:44 -0600

    Make the website optimized for mobile

--- template.php
+++ template.php
@@ -24,6 +24,7 @@
 <head>
   <title><?php echo $articleTitle; ?></title>
   <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <!-- other meta, CSS, and custom tags -->
 
   <!-- Bootstrap 4 CSS -->
