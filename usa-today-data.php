06da69d2d915e0d5d33a516ce4b53cd8210ec7c3
Levi Villarreal <villarreallevi@gmail.com>
2018-11-27 16:26:22 -0600

    Add USA today data

--- usa-today-data.php
+++ usa-today-data.php
@@ -1,24 +1,59 @@
 <?php
 $org = 'USA Today'; // Either 'USA Today' or 'Tennessean'
 
-$articleTitle = 'Trump calls for limiting asylum claims at border as he pushes immigration issue ahead of midterms';
-$articleAuthor = 'David Jackson';
-$articleImage =  'assets/img/article-image.jpg';
-$articleCaption = '(Photo: Photo courtesy Steve West via stevewest4missouri.com)';
-$articlePublishDate = '11:16 a.m. ET Nov. 1, 2018';
-$articleUpdateDate = ' | Updated 4:28 p.m. ET Nov. 1, 2018';
+$articleTitle = 'Was Amazon\'s headquarters search \'a giant ruse\'? NYC, D.C. centers of power prevail';
+$articleAuthor = 'Nathan Bomey';
+$articleImage =  'assets/img/usa-today-article-image.jpg';
+$articleCaption = '(Photo: DON EMMERT, AFP/Getty Images)';
+$articlePublishDate = '11:07 a.m. ET Nov. 13, 2018';
+$articleUpdateDate = ' | Updated 2:23 p.m. ET Nov. 13, 2018';
 
 $articleText = '
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur blanditiis laboriosam tempore atque nobis commodi temporibus eos, facere minima ea officiis ratione quam cupiditate vel incidunt alias, corporis accusamus nisi.</p>';
+  <p>Amazon\'s selection of New York City and Arlington, Virginia, for its new headquarters – as well as Nashville, Tennessee, for a major investment – leaves major cities throughout the country wondering what could have been.</p>
 
-  $eypText = '';
+  <p>As Amazon dangled the prospect of 50,000 jobs and economic transformation, virtually every major city in the country jockeyed to win an economic development race for the ages.</p>
+
+  <p>But cities such as Chicago, Denver, Pittsburgh, Phoenix, Miami and Los Angeles were ultimately left empty-handed.</p>
+
+  <p>Did those cities get played? Did they ever have a legitimate shot at winning this race?</p>
+
+  <p>Critics say Amazon exploited the list of contenders for the sake of learning more about their secret plans for infrastructure investments, economic development and tax incentives.</p>
+
+  <p>And it\'s hard to escape the fact that after about 14 months of consideration, Amazon ended up picking two of the richest areas in the country: New York and Washington, D.C.</p>
+
+  <p>While D.C. technically didn\'t win the so-called HQ2 sweepstakes – Arlington is just across the Potomac River – the District will inevitably benefit from the halo effect of Amazon establishing what it called its "new Washington, D.C., metro headquarters."</p>
+
+  <p>"Cities all around the country bent over backwards to try to apply," said Calandra Cruickshank, CEO of StateBook International, a provider of economic development data. "Amazon certainly got a wealth of information about what properties and incentives and attributes different communities have to offer."</p>
+
+  <p>Critics say Amazon can use that information to make future investment decisions. But Cruickshank defended Amazon\'s process, saying that ultimately many cities that submitted applications benefited from the process. The process enabled those applicants to refine their pitches to prospective investors and identify areas where they need to improve, such as transit, labor and quality of life, she said.</p>
+
+  <p>"I feel like the process was pretty carefully done, and these other communities really did have a chance," Cruickshank said.</p>
+
+  <p>Resentment, however, is already building.</p>
+
+  <p>Amazon critic Stacy Mitchell, co-director of the Institute for Local Self-Reliance, called the search process "a giant ruse" and said the selection of Nashville, Tennessee, for 5,000 jobs is "a head-fake" that will fool "many journalists" into not applying appropriate scrutiny to the online giant.</p>
+
+  <p>One alternative view: Cities that lost actually won. Critics of tax incentives say cities shouldn\'t hand over the kitchen sink to corporations like Amazon.</p>
+
+  <p>"Despite arguments in support of economic development subsidies, both theory and experience suggest that cities and states are throwing their money away when they court Amazon’s favor through subsidies," wrote Michael Farren, research fellow at George Mason University\'s Mercatus Center, which conducts research promoting free market policies, in a report. </p>
+
+  <p>He said "these kinds of targeted economic development incentives fail to produce economic growth."</p>
+
+  <p>Amazon did not immediately respond to requests seeking comment.</p>
+
+  <p>“We are excited to build new headquarters in New York City and Northern Virginia,” Amazon CEO Jeff Bezos said in a statement. “These two locations will allow us to attract world-class talent that will help us to continue inventing for customers for years to come. The team did a great job selecting these sites, and we look forward to becoming an even bigger part of these communities.”</p>
+
+  <p>Contributing: Elizabeth Weise</p>';
+
+  $eypText = '
+  <h4>Why we’re doing this story:</h4>
+
+  <p>The decision of where Amazon put its new headquarters is big economic news for both the two cities that were named and the 18 that had wanted but didn’t get it. We wanted to explain what Amazon had decided, <a class="link">why the 18 other cities most likely didn\'t get the nod</a> (Amazon wasn’t saying) and why some people were saying the entire process was a scam on Amazon’s part.</p>
+
+  <h4>How we reported this story:</h4>
+
+  <p>Our reporting team has been covering this story since 2017. We knew by August of 2018 that Amazon had done at least the first round of visits to the 20 finalists, so we started interviewing officials in the most likely cities, talking to economic development experts about the potential impact of having Amazon settle in your town and looking into the amount of tax incentives each area had offered.</p>
+
+  <h4>How we took steps to be fair:</h4>
+
+  <p>Neither Amazon nor any of the 20 finalist cities would talk about the process. In fact, Amazon required that cities not release information. In addition, most of the cities did not reveal anything about how much money they were offering Amazon in tax and other incentives. Because of that, we had to rely on experts who were offering their opinions without having access to all the facts. We talked with economic development experts who thought having Amazon come to a city would be positive for the area as well as groups that felt the economic incentives being offered were so much that many people currently living in those cities would have suffered if Amazon set up shop there. Those concerns were mostly about gentrification, rising housing prices and congestion.</p>';
